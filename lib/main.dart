e944b2a595527fcc84d8d873312e20f263101796
liu zhen <like7th@gmail.com>
2021-06-22 17:47:35 +0800

    列表增加了测试数组

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,50 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+
+  var list = [
+    ["排序", "冒泡排序，快速排序，选择排序，插入排序，希尔排序，堆排序"],
+    ["栈", "栈内容"],
+    ["队列", "队列内容"],
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'memory curve',
-      theme: new ThemeData(
-        primaryColor: Colors.lightBlue,
-        primarySwatch: Colors.lightGreen,
-      ),
-      home: new Scaffold(//增加Scaffold后背景为白色，否则为黑
-        appBar: new AppBar(
-          title: new Text("memory"),
 
+    return new MaterialApp(
+        title: 'memory curve',
+        theme: new ThemeData(
+          primaryColor: Colors.lightBlue,
+          primarySwatch: Colors.lightGreen,
         ),
-        body: new ListView.builder(
+        home: new Scaffold(
+          //增加Scaffold后背景为白色，否则为黑
+          appBar: new AppBar(
+            title: new Text("memory"),
+          ),
+          body: new ListView.separated(
             shrinkWrap: true,
             padding: const EdgeInsets.all(32.0),
-            itemBuilder : (BuildContext context, int index) {
-              return ListTile(title: Text('$index'));
-            },
-            itemCount : 100,
-            itemExtent: 35,
-        ),
-      )
-    );
-  }
+            itemCount: list.length,
+            //列表项构造器
+            itemBuilder: (BuildContext context, int index) {
+              // return ListTile(title: Text('$index'));
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
 
+                  Text(list[index][0],style: TextStyle(fontSize: 18)),
+                  Text(list[index][1])
+                ],
+              );
+            },
+            //分割器构造器
+            separatorBuilder: (BuildContext context, int index) {
+              return Divider(color: Colors.blue);//下划线
+            },
+            // itemExtent: 60,//可以控制高度
+          ),
+        ));
+  }
 }
