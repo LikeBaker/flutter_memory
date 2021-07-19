60660f665ce86dfc3d2aae7a81a0c2d251059ab5
liu zhen <like7th@gmail.com>
2021-07-19 15:51:17 +0800

    edit page:获取到数据库数据

--- lib/Edit.dart
+++ lib/Edit.dart
@@ -8,7 +8,30 @@ class EditPage extends StatelessWidget{
   var dbHelper;
 
   EditPage() {
+    init();
+  }
+
+  void init() async{
     dbHelper = DbHelper();
+    await dbHelper.initializationDone;
+    refreshData();
+  }
+
+  var list = <Memory>[];
+
+  Future<void> refreshData() async {
+    var memory = await dbHelper.getMemories();
+    for (int i = 0; i < memory.length; i++) {
+      var m = memory[i];
+      print(m.toString());
+      // if (isShow(m)) {
+      //   list.add(m);
+      // }
+      list.add(m);
+    }
+
+    // ignore: invalid_use_of_protected_member
+    print("edit refresh");
   }
 
   @override
