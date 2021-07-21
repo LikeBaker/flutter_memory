7a768c2ca52c31007cf420b369a0d9769b7b9551
liu zhen <like7th@gmail.com>
2021-07-21 16:45:51 +0800

    编辑页面：将页面编辑整齐

--- lib/Edit.dart
+++ lib/Edit.dart
@@ -47,38 +47,35 @@ class _EditPageState extends State<EditPage> {
     var ls = new ListView.separated(
         shrinkWrap: true,
         itemBuilder: (context, index) {
-          var title =
-          Row(children: [
-              Checkbox(onChanged:null, value: true),
-              Container
-          (
+          var title = Container(
               padding: EdgeInsets.all(16.0),
-          alignment: Alignment.centerLeft,
-          child: Text(_list[index].title))
-          ]);
-
+              alignment: Alignment.topLeft,
+              color: Colors.pink,
+              child: Text(_list[index].title));
           // var markdownWidget = Markdown(data: _list[index].content);
-          var markdownWidget = MarkdownBody(
-          data: _list[index]
-              .content); //这里使用MarkdownBody，可以自适应高度，否则Markdown必须指定高度
-          return new Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-          title,
-          //mark widget 必须包裹一层
-          // Stack(
-          //   children: [
-          Container(
-          padding: EdgeInsets.all(16.0), alignment: Alignment.centerLeft,
-          color: Colors.lightBlueAccent,
-          // child: markdownWidget,
-          child: markdownWidget,
-          )
-          // ],
-          // )
-          ]
-          ,
-          );
+          //这里使用MarkdownBody，可以自适应高度，否则Markdown必须指定高度
+          // var markdownWidget = MarkdownBody(data: _list[index].content);
+          var markdownWidget = MarkdownBody(data:_list[index].content);
+          var memories = Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // mainAxisSize: MainAxisSize.min,
+              children: [
+                Checkbox(
+                    onChanged: (value) => {print("$index $value")},
+                    value: false),
+                Expanded(child: new Column(crossAxisAlignment:CrossAxisAlignment.start, children: [
+                  title,
+                  Container(
+                    // width: 100,
+                    // height: 100,
+                      padding: EdgeInsets.all(16.0),
+                      alignment: Alignment.centerLeft,
+                      color: Colors.lightBlueAccent,
+                      child: markdownWidget
+                  )
+                ]))
+              ]);
+          return memories;
         },
         separatorBuilder: (context, index) => divider,
         itemCount: _list.length);
